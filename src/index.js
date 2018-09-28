2af59c3f10fbe0e4f91f9c99a7942e284591d61b
Meearlyam <shavela99@gmail.com>
2018-09-28 12:25:26 +0300

    sec

--- src/index.js
+++ src/index.js
@@ -9,33 +9,34 @@ module.exports = function check(str, bracketsConfig) {
   }
   
   let first = br_array.shift();
-
-  while(open_brackets.indexOf(first) == -1) {
-    if(close_brackets.indexOf(first) != -1) {
-      return false;
-    }
-  }
+  if((close_brackets.indexOf(first) != -1) && (open_brackets.indexOf(first) == -1)) {
+    return false;
+  } 
 
   let control_stack = [];
   control_stack.push(first);
-
-  for(let i = 0, len = br_array.length; i < len; i++){
-    if(open_brackets.indexOf(br_array[i]) != -1) {
-      control_stack.push(br_array[i]);
-    }
-    else {
-      while((close_brackets.indexOf(br_array[i]) == -1) && (open_brackets.indexOf(br_array[i]) == -1)){
-        i++;
+  let current, last_added;
+  
+  while(br_array.length != 0) {
+    current = br_array.shift();
+    last_added = control_stack.pop();
+    if (open_brackets.indexOf(current) != -1) {                                      //is open bracket
+      if(close_brackets.indexOf(current) == -1) {                                    //is not close bracket
+        control_stack.push(last_added);                                  
+        control_stack.push(current);
       }
-      if(close_brackets.indexOf(br_array[i]) != open_brackets.indexOf(control_stack.pop())){
+      else {                                                                         //is close bracket
+        if (open_brackets.indexOf(last_added) != close_brackets.indexOf(current)) {  //are they twins with last added?
+          control_stack.push(last_added);
+          control_stack.push(current);
+        }
+      }
+    }
+    else {                                                                           //is close bracket
+      if(open_brackets.indexOf(last_added) != close_brackets.indexOf(current)) {
         return false;
       }
     }
   }
-  if(control_stack.length == 0) {
-    return true;
-  }
-  else {
-    return false;
-  }
+  return true;
 }
